ff8387ae1b3b6b14c12a06f5b59b91119465cb81
Seneng Utami <senengutami2@gmail.com>
2023-03-01 14:04:13 +0545

    update the temperature description

--- shecode-plus/week-4-and-5/homework/src/index.js
+++ shecode-plus/week-4-and-5/homework/src/index.js
@@ -11,9 +11,9 @@ let dayNames = [
   "Saturday",
 ];
 let dayOfWeek = dayNames[now.getDay()];
-let hour = now.getHours();
 
 // let hour
+let hour = now.getHours();
 if (hour < 10) {
   hour = `0${hour}`;
 }
@@ -23,6 +23,7 @@ let minute = now.getMinutes();
 if (minute < 10) {
   minute = `0${minute}`;
 }
+
 let currentTime = dayOfWeek + " " + hour + ":" + minute;
 
 console.log(currentTime); // or display on your HTML page with document.write(currentTime);
@@ -30,17 +31,20 @@ console.log(currentTime); // or display on your HTML page with document.write(cu
 let daySelector = document.querySelector("#day");
 daySelector.innerHTML = currentTime;
 
-//
+// temperature
 function showTemperature(response) {
   console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#number");
   temperatureElement.innerHTML = `${temperature}`;
 
-  //   update city name
-
+  // update city name
   let currentCity = document.querySelector("#cityName");
   currentCity.innerHTML = response.data.name;
+
+  // update the temperature-description
+  let description = document.querySelector("#temperature-description");
+  description.innerHTML = response.data.weather[0].description;
 }
 function search(event) {
   event.preventDefault();
@@ -53,11 +57,8 @@ function search(event) {
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
   city.value = "";
 }
-let form = document.querySelector("#search");
-form.addEventListener("click", search);
 
 function showPosition(position) {
-  let h2 = document.getElementById("current-latitude-longitude");
   console.log(position.coords.latitude);
   console.log(position.coords.longitude);
   let apiUrlLat = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`;
@@ -69,46 +70,8 @@ function getCurrentPosition(event) {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-let current = document.querySelector("#current");
-current.addEventListener("click", getCurrentPosition);
+let currentBut = document.querySelector("#current");
+currentBut.addEventListener("click", getCurrentPosition);
 
-// let cel = document.querySelector("#celcius");
-// cel.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   let num = document.querySelector("#number");
-//   num.innerHTML = "23";
-// });
-
-// let fah = document.querySelector("#fahrenheit");
-// fah.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   let num = document.querySelector("#number");
-//   num.innerHTML = "73";
-// });
-
-// search engine
-
-// let apiKey = "906649fdf112556a043b2efecf09654c";
-// // let city = "Sydney";
-// let apiUrl =
-//   `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-
-// // Make a request for a user with a given ID
-// function showTemperature(response) {
-//   console.log(response.data);
-//   let temperature = Math.round(response.data.main.temp);
-//   let temperatureElement = document.querySelector("#number");
-//   temperatureElement.innerHTML = `${temperature}`;
-
-// }
-
-// let h2 = document.querySelector("#cityName");
-// h2.innerHTML = city;
-
-// axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
-
-// 1. addEventListener to button
-// 2. call search function
-// 3. inside search function update the city display name
-// 4. with the same city name call weather api from inside the function
-// 5.
+let searchBut = document.querySelector("#search");
+searchBut.addEventListener("click", search);
